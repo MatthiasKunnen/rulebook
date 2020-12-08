3c92e7b114d7137377073244cf4162cb44e2b6a8
Matthias Kunnen <matthias.kunnen@gmail.com>
2020-12-08 21:55:17 +0000

    Add script to find TS specific rule anomalies

---
+++ linters/eslint/typescript/check-typescript-rules.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const path = require('path');
+
+const baseConfig = require('../base/index');
+const typeScriptConfig = require('./index');
+
+const typeScriptRulesPath = path.join(
+    __dirname,
+    'node_modules',
+    '@typescript-eslint',
+    'eslint-plugin',
+    'dist',
+    'rules',
+);
+
+const typeScriptRules = new Set(fs.readdirSync(typeScriptRulesPath)
+    .filter(rule => path.extname(rule) === '.js')
+    .map(rule => path.basename(rule, path.extname(rule))));
+
+const output = {
+    baseNotTurnedOff: [],
+    unused: [],
+    unusedButBaseTurnedOff: [],
+};
+
+Object.keys(baseConfig.rules).forEach(rule => {
+    if (typeScriptRules.has(rule)) {
+        const isTurnedOff = typeScriptConfig.rules[rule] === 'off';
+        const isUsed = typeScriptConfig.rules[`@typescript-eslint/${rule}`] !== undefined;
+
+        if (isUsed && !isTurnedOff) {
+            output.baseNotTurnedOff.push(rule);
+        } else if (isTurnedOff && !isUsed) {
+            output.unusedButBaseTurnedOff.push(rule);
+        } else if (!isUsed) {
+            output.unused.push(rule);
+        }
+    }
+});
+
+console.log(JSON.stringify(output, undefined, 4));
